1f360564f0360a21e8afbb9d220c05fc90e72b5b
n37r09u3 <n37r09u3@gmail.com>
2022-02-02 19:08:44 +0800

    3-16

---
+++ utils/ex.js
@@ -0,0 +1,37 @@
+const CODE = {
+    SUCCESS: 200,
+    PARAM_ERROR: 10001,
+    USER_ACCOUNT_ERROR: 20001,
+    USER_LOGIN_ERROR: 30001,
+    BUSINESS_ERROR: 40001,
+    AUTH_ERROR: 50001,
+
+}
+
+
+module.exports = {
+    pager({pageNum=1,pageSize=10}){
+        pageNum*=1;
+        pageSize*=1;
+        const skipIndex = (pageNum-1)*pageSize;
+        return {
+            page:{
+                pageNum,
+                pageSize
+            },
+            skipIndex
+        }
+    },
+    success(data='', msg='', code=CODE.SUCCESS){
+        return {
+            code, data, msg
+        }
+    },
+    fail(msg='', code=CODE.BUSINESS_ERROR){
+        return {
+            code, data, msg
+        }
+    },
+
+
+}
